a3e2edd682ac1c353cae6af0e317125bb7dc9446
jo <megatron24100@gmail.com>
2026-09-14 17:50:44 +0300

    create user.reposityr file

---
+++ src/DB/repository/user.repository.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common";
+import { UserDocument as TDocument, User } from "../model/user.model";
+import { DatabaseRepository } from "./database.repository";
+import { Model } from "mongoose";
+import { InjectModel } from "@nestjs/mongoose";
+
+@Injectable()
+export class UserRepository extends DatabaseRepository<TDocument> {
+    constructor(@InjectModel(User.name) protected readonly model: Model<TDocument>) {
+        super(model);
+    }
+}
